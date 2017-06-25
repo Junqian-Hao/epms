32caaa8f2daf834a6a4bf5fc5297272e2a06a414
16359 <1635920137@qq.com>
2017-06-25 17:39:06 +0800

    更正全局异常处理

--- src/cn/edu/nuc/epms/intercepts/AllExceptionResolver.java
+++ src/cn/edu/nuc/epms/intercepts/AllExceptionResolver.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * @Author 郝俊谦
@@ -21,12 +20,6 @@ public class AllExceptionResolver implements HandlerExceptionResolver {
                                          HttpServletResponse httpServletResponse,
                                          Object o, Exception e) {
         log.debug("全局异常处理");
-        try {
-
-            httpServletResponse.sendRedirect("error.html");
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-        return null;
+        return new ModelAndView("/error.html");
     }
 }
